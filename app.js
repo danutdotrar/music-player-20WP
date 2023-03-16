0c98cf356d4990b99d23e06b9cf5f71b529dd9f4
danutdotrar <morarasu.danut@gmail.com>
2023-03-16 10:52:21 +0200

    Add setProgress fn

--- app.js
+++ app.js
@@ -77,6 +77,15 @@ function updateProgress(e) {
     progress.style.width = `${progressPercent}%`;
 }
 
+// Set progress
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
 // Event Listeners
 playBtn.addEventListener("click", () => {
     const isPlaying = musicContainer.classList.contains("play");
